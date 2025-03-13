14e3f694f1e5c05f25bbea6496acc583f4a2d587
researchafrica <140722371+researchafrica@users.noreply.github.com>
2025-03-13 12:43:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector('#NavBar');
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
+z
